ff24ebf55ea75ed0ecb7c8b61456dc37f37df57d
iitzex <iitzex@gmail.com>
2016-10-29 15:53:43 +0800

    update main, sub, latlon

--- db.py
+++ db.py
@@ -7,14 +7,14 @@ Base = declarative_base()
 
 
 class Store(Base):
-    __tablename__ = 'stores'
+    __tablename__ = 'store'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     phone = Column(String(20))
     address = Column(String(50))
     city = Column(String(20))
     town = Column(String(20))
-    category = Column(String(20))
+    main = Column(String(20))
     sub = Column(String(20))
     lat = Column(Float(20))
     lon = Column(Float(20))
@@ -45,7 +45,6 @@ def test():
         print(i)
         # session.delete(i)
     session.commit()
-
 
 if __name__ == '__main__':
     init()
--- travel.py
+++ travel.py
@@ -8,76 +8,79 @@ from sqlalchemy.orm import sessionmaker
 from db import Store
 
 
-def detail(ADDR):
-    r = requests.get(ADDR)
-    content_big5 = r.content.decode('cp950').encode('utf-8')
+def detail(addr):
+    r = requests.get(addr)
+    content = r.content.decode('cp950')
+    soup = BeautifulSoup(content, "html.parser")
+    all_td = soup.find_all('td', align='right')
+
+    res = [None] * 3
+
+    for td in all_td:
+        if td.get_text() == '縣市名稱':
+            res[0] = td.find_next_sibling('td').get_text().strip().replace('\\xa0', ' ')
+        if td.get_text() == '鄉鎮名稱':
+            res[1] = td.find_next_sibling('td').get_text().strip().replace('\\xa0', ' ')
+        # if td.get_text() == '行業別':
+        #     res[2] = td.find_next_sibling('td').get_text().strip().replace('\\xa0', ' ')
+        if td.get_text() == '行業細項分類':
+            res[2] = td.find_next_sibling('td').get_text().strip().replace('\\xa0', ' ')
+
+    return res
+
+
+def traverse(addr, map_nccc, session):
+    retailer = requests.get(addr)
+    content_big5 = retailer.content.decode('cp950')
     soup = BeautifulSoup(content_big5, "html.parser")
-    menu = soup.find_all('table', bgcolor='#FAFAF5')
-    all_tr = menu[0].find_all('tr')
+    table = soup.find('table', bgcolor='#fafaf5')
 
-    main_addr = ''
-    sub_addr =''
-
-    for tr in all_tr:
-        td = tr.find_all('td')
-        if td[0].get_text() == '縣市名稱' and len(td) > 1:
-            main_addr = td[1].get_text()
-        if td[0].get_text() == '鄉鎮名稱' and len(td) > 1:
-            sub_addr = td[1].get_text()
-
-    return main_addr, sub_addr
-
-
-def traverse(ADDR, map_osm, session):
-    retailer = requests.get(ADDR)
-    content_big5 = retailer.content.decode('cp950').encode('utf-8')
-    soup = BeautifulSoup(content_big5, "html.parser")
-    menu = soup.find_all('table', bgcolor='#fafaf5')
-
-    all_tr = menu[0].tr.find_all('tr', valign='top')
+    all_tr = table.tr.find_all('tr', valign='top')
     for i, each in enumerate(all_tr):
         item = each.find_all('td')
 
         name = item[0].get_text().strip()
+        main = item[1].get_text().strip()
         phone = item[2].get_text().strip()
         address = item[3].get_text().strip()
-        link = item[5].a
-        href = 'http://travel.nccc.com.tw'+link['href']
-        # print href
-        main, sub = detail(href)
-        main = main.strip().replace('\\xa0', ' ')
-        sub = sub.strip().replace('\\xa0', ' ')
+        href = 'http://travel.nccc.com.tw'+item[5].a['href']
+        city, town, sub = detail(href)
 
-        # print(i+1, name, phone, category, main, sub, address)
-        print(i+1, name, address)
-        retailer = Store(name=name, phone=phone, address=address, category=main, sub=sub)
+        print(i+1, name, phone, city, town, address, main, sub)
+        retailer = Store(name=name, phone=phone, address=address, city=city, town=town,
+                         main=main, sub=sub)
 
         latlon = locator(address)
         if latlon is not None:
-            folium.Marker(location=latlon, popup=name).add_to(map_osm)
-            map_osm.location = latlon
-            map_osm.save('nccc.html')
+            folium.Marker(location=latlon, popup=name).add_to(map_nccc)
+            map_nccc.location = latlon
+            map_nccc.save('nccc.html')
 
             print('\t' + str(latlon))
+
             retailer.lat = latlon[0]
             retailer.lon = latlon[1]
+
+            res = session.query(Store).filter_by(name=name).all()
+            if res is None:
+                res.lat = latlon[0]
+                res.lon = latlon[1]
+                session.commit()
+                print('update latlon', res)
+                continue
 
         session.add(retailer)
     session.commit()
 
-    next_a = menu[0].previous_element.previous_element.previous_element
+    next_a = table.previous_element.previous_element.previous_element
     next_href = 'http://travel.nccc.com.tw'+next_a['href']
     print(next_href)
-    tag = next_a.get_text()
-    if tag == '下一頁':
-        traverse(next_href, map_osm, session)
+    if next_a.get_text() == '下一頁':
+        traverse(next_href, map_nccc, session)
 
 
 def locator(place):
-    # g = geocoder.google(place)
     g = geocoder.google(place)
-    g = geocoder.google('Mountain View, CA')
-    print(g.latlng)
     if len(g.latlng) is 0:
         return
 
@@ -85,27 +88,34 @@ def locator(place):
 
 
 def travel(location):
-    ADDR = 'http://travel.nccc.com.tw/NASApp/NTC/servlet/com.du.mvc.Entry' \
+    addr = 'http://travel.nccc.com.tw/NASApp/NTC/servlet/com.du.mvc.Entry' \
            'Servlet?Action=RetailerList&Type=GetFull&Request=NULL_NULL_NULL_' \
            +location+ \
            '_NULL_NULL_NULL_NULL_NULL_0_1_20_99999'
-    print(ADDR)
+    print(addr)
 
     try:
-        map_osm = folium.Map(zoom_start=13, tiles='CartoDB positron')
-        engine = create_engine('sqlite:///nccc.db', echo=True)
-        DBSession = sessionmaker(bind=engine)
-        session = DBSession()
+        map_nccc = folium.Map(zoom_start=13, tiles='CartoDB positron')
+        session = connect_db()
 
-        traverse(ADDR, map_osm, session)
+        traverse(addr, map_nccc, session)
     except IndexError as e:
         print(e)
-        pass
+    except AttributeError as e:
+        print(e)
+
+
+def connect_db():
+    engine = create_engine('sqlite:///nccc.db', echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
 
 
 if __name__ == '__main__':
     location = '025'
-    location = '002'
+    # location = '002'
 
-    # travel(location)
-    locator('基隆市仁愛區和明里仁一路２９５號１樓')
+    travel(location)
+
+
